7bccbe8e4bb15fa8582260cc579acdadf37671ff
Ahmer Naseer <ahmerkhawaja16414@gmail.com>
2022-02-13 02:07:11 +0500

    header

--- src/index.js
+++ src/index.js
@@ -20,17 +20,28 @@ export default function app() {
     const obj = { updateTitle:updateTitle};
     ///////////////////////////////////////////
 
+                    const Headerupdate = ({titl}) => {
+                    if(titl!=='Edits'){
+                        return( <View style={styles.header}>
+                            <Text style={styles.content}>{titl}</Text>
+                        </View>)
+                   
+}
+                    else if(titl=='Edits'){
+                        return( <View style={styles.header}>
+                           <Icon style={styles.content} name='align-justify'  color='white' />
+                            <Text style={styles.content}>{titl}</Text>
+                           <Icon style={styles.content} name='search'  color='white' />  
+                           </View>)
+                            }
+}
+
+
     return (
         <TitleContext.Provider value={obj}>
             <View style={{ flex: 1 }} >
-                <StatusBar
-                    backgroundColor='#084b9e'
-                />
-                <View style={styles.header}>
-                    <Icon name='align-justify'  color='white' />
-                    <Text>{title}</Text>
-                    <Icon name='search'  color='white' />
-                </View>
+                <StatusBar backgroundColor='#084b9e'/>
+                <Headerupdate titl={title}/>
                 <AppIndex/>
             </View>
         </TitleContext.Provider>
@@ -38,9 +49,6 @@ export default function app() {
 
 }
 const styles = StyleSheet.create({
-    wrapper: {
-        flex: 1,
-    },
     header: {
         flexDirection: 'row',
         alignItems: 'center',
@@ -49,5 +57,12 @@ const styles = StyleSheet.create({
         paddingHorizontal: 18,
         paddingTop: 18,
         paddingBottom: 18,
+       
+    },
+    content:{ fontSize:20,
+        color:'white',
+        paddingLeft:2,
+        paddingRight:2,
     }
+    
 });
